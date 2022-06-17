475ed8d81f48a4b8d422b4f752068a471c3ec24a
Zamecki <mateuszamecki@hotmail.com>
2022-06-17 16:19:57 -0400

    Teste logico

---
+++ NetWork.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteLogico
+{
+    public class NetWork
+    {
+        public void Connect(List<Node> nodes, int previus, int next)
+        {
+            var node = nodes.Where(node => node.Value == previus).FirstOrDefault();
+            var nextNode = nodes.Where(node => node.Value == next).FirstOrDefault();
+
+            if (node.Next == null)
+            {
+                var newNode = new Node(nextNode.Value);
+                node.Next = newNode;
+            }
+            else
+            {
+                var newNode = new Node(node.Value);
+                newNode.Next = new Node(nextNode.Value);
+                nodes.Add(newNode);
+            }
+
+            if (nextNode.Next == null)
+            {
+                var newNode = new Node(node.Value);
+                nextNode.Next = newNode;
+            }
+            else
+            {
+                var newNode = new Node(nextNode.Value);
+                newNode.Next = new Node(node.Value);
+                nodes.Add(newNode);
+            }
+        }
+
+        public bool Query(int goal, List<Node> nodes, List<int> traveledNodes, Node currentNode)
+        {
+
+            if (currentNode.Next == null)
+            {
+                return false;
+            }
+            if (currentNode.Next.Value == goal)
+            {
+                return true;
+            }
+            var filtredNodes = nodes.Where(node => node.Value == currentNode.Next.Value && !traveledNodes.Contains(node.Value)).ToList();
+
+            foreach (var node in filtredNodes)
+            {
+                if (node.Next != null)
+                {
+                    traveledNodes.Add(node.Value);
+                    var res = Query(goal, nodes, traveledNodes, node);
+                    if (res)
+                    {
+                        return res;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TesteLogico
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var network = new NetWork();
+            var nodes = new List<Node>();
+
+            Console.WriteLine("Enter a quantity of elements");
+            int number = int.Parse(Console.ReadLine());
+            for (int count = 1; count <= number; count++)
+            {
+                nodes.Add(new Node(count));
+            }
+
+            Console.WriteLine("______________Connect______________  ");
+            
+            var exit = 1;
+            while (exit != 0)
+            {
+                Console.WriteLine("___________First element___________");
+                var previus = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("___________Second element__________");
+                var next = int.Parse(Console.ReadLine());
+
+                if (previus > number || next > number)
+                {
+                    throw new Exception("Invalid Element");
+                }
+
+                network.Connect(nodes, previus, next);
+                nodes = nodes.OrderBy(node => node.Value).ToList();
+
+
+                Console.WriteLine("______________Exit______________: 0");
+                exit = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("______________Query______________");
+            var exitQuery = 1;
+
+            while (exitQuery != 0)
+            {
+                Console.WriteLine("___________First element___________");
+                var first = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("___________Second element__________");
+                var goal = int.Parse(Console.ReadLine());
+
+                if (first > number || goal > number)
+                {
+                    throw new Exception("Invalid Element");
+                }
+
+                var currentNode = nodes.Where(node => node.Value == first).FirstOrDefault();
+                var response = network.Query(goal, nodes, new List<int>(), currentNode);
+
+
+                Console.WriteLine("_________Response: ", response);
+                Console.WriteLine("______________Exit______________: 0");
+
+                exitQuery = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
